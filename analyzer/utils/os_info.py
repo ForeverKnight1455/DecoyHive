feea3e850581f66f3899245dcb6de255750633dd
Sreyas P Pradeep <sreyasinemily@gmail.com>
2025-03-06 14:29:57 +0530

    made changes in os_info.py

--- analyzer/utils/os_info.py
+++ analyzer/utils/os_info.py
@@ -8,5 +8,4 @@ def get_os_info():
         "os_version": platform.release(),
         "os_architecture": platform.machine(),
         "kernel_version": platform.version(),
-        "installed_packages": subprocess.run(["dpkg", "-l"], capture_output=True, text=True).stdout
     }
